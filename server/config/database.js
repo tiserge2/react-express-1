6ef924276c13b541f6af3000199538f463e44c16
Osson Sergio Suzerain <sergioosson@gmail.com>
2021-07-10 00:30:31 -0400

    fixing db issue with atlas

--- server/config/database.js
+++ server/config/database.js
@@ -2,5 +2,7 @@ module.exports = {
     //configuration for local database
     "local_url" : "mongodb://localhost/musicTracking",
     //configuration for online database
-    "online_url" : "mongodb://tiserge2:tiserge2@ds111430.mlab.com:11430/utility"
+    "online_url_old" : "mongodb://tiserge2:tiserge2@ds111430.mlab.com:11430/utility",
+    //configuration for new online url
+    "online_url": "mongodb+srv://root:jsuR5RLFJ55Ql1yl@cluster0.uibil.mongodb.net/utility?retryWrites=true&w=majority"
 }
