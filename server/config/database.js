bc1e0be6f175fa5cc03e00d62b7532c4236081f1
Osson Sergio Suzerain <sergioosson@gmail.com>
2023-06-03 15:00:31 +0200

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -4,5 +4,5 @@ module.exports = {
     //configuration for online database
     "online_url_old" : "mongodb://tiserge2:tiserge2@ds111430.mlab.com:11430/utility",
     //configuration for new online url
-    "online_url": "mongodb+srv://root:jsuR5RLFJ55Ql1yl@cluster0.uibil.mongodb.net/utility?retryWrites=true&w=majority"
+    "online_url": "mongodb+srv://root:jsuR5RLFJ55Ql1yl@cluster0.wr3b1.mongodb.net/utility?retryWrites=true&w=majority"
 }
